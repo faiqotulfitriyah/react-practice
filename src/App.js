a2e36502c4dafeadda5bf085dd492e9be43c9c85
Faiqotul <faiqotulftryh@gmail.com>
2023-08-29 10:58:29 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Routes, Route, Switch, Link } from 'react-router-dom';
+import BMICalculator from './components/BMICalcu';
+import ToDoList from './components/ToDoList';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Routes>
+			<Switch>
+			<Route exact path="/">
+				<h1>React Best Practices</h1>
+			</Route>
+			<Route path="/bmi">
+				<BMICalculator />
+			</Route>
+			<Route path="/todo">
+				<ToDoList />
+			</Route>
+			</Switch>
+		<div className='Route-container'>
+			<nav className='Navbar-nav'>
+				<ul className='list-ul'>
+					<li>
+					<Link to="/">Home</Link>
+					</li>
+					<li>
+					<Link to="/bmi">BMI Calculator</Link>
+					</li>
+					<li>
+					<Link to="/todo">To-Do List</Link>
+					</li>
+				</ul>
+			</nav>
+
+		</div>
+		</Routes>
+	);	
 }
 
 export default App;
---
+++ src/components/BMICalcu.js
@@ -0,0 +1,77 @@
+import React, { useState } from 'react';
+import './Bmi.css';
+
+function BmiCalculator() {
+	const [heightValue, setHeightValue] = useState('');
+	const [weightValue, setWeightValue] = useState('');
+	const [bmiValue, setBmiValue] = useState('');
+	const [bmiMessage, setBmiMessage] = useState('');
+
+	const calculateBmi = () => {
+		if (heightValue && weightValue) {
+			const heightInMeters = heightValue / 100;
+			const bmi = (weightValue / (heightInMeters * heightInMeters)).toFixed(2);
+			setBmiValue(bmi);
+
+			let message = '';
+			if (bmi < 18.5) {
+				message = 'You are Underweight';
+			} else if (bmi >= 18.5 && bmi < 25) {
+				message = 'You are Normal weight';
+			} else if (bmi >= 25 && bmi < 30) {
+				message = 'You are Overweight';
+			} else {
+				message = 'You are Obese';
+			}
+			setBmiMessage(message);
+		} else {
+			setBmiValue('');
+			setBmiMessage('');
+		}
+
+	};
+
+	return (
+		<div className="container">
+      <br></br>
+			<h1>Fiqa & Bryant's <br></br> BMI Calculator</h1>
+      <img src='https://upload.wikimedia.org/wikipedia/commons/0/0d/Logo_Vidio.png' className='logo'/>
+			<div className="input-container">
+        <br></br>
+				<label htmlFor="height">Enter Your Height (cm):</label>
+				<input
+					type="number"
+					min="0"
+					id="height"
+					value={heightValue}
+					onChange={(e) => setHeightValue(e.target.value)}
+				/>
+			</div>
+			<div className="input-container">
+				<label htmlFor="weight">Enter Your Weight (kg):</label>
+				<input
+					type="number"
+					min="0"
+					id="weight"
+					value={weightValue}
+					onChange={(e) => setWeightValue(e.target.value)}
+				/>
+			</div>
+			<button className="calculate-btn" onClick={calculateBmi}>
+				Calculate Your BMI
+			</button>
+			{bmiValue && bmiMessage && (
+				<div className="result">
+					<p>
+						Your BMI: <span className="bmi-value">{bmiValue}</span>
+					</p>
+					<p>
+						Result: <span className="bmi-message">{bmiMessage}</span>
+					</p>
+				</div>
+			)}
+		</div>
+	);
+}
+
+export default BmiCalculator;
